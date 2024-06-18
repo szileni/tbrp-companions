d4cab3e8ff8106375cc308d81bb9a8ebcaa866cb
szileni <szileni97@gmail.com>
2024-06-18 21:47:56 +0200

    Create npcs.lua

---
+++ client/npcs.lua
@@ -0,0 +1,65 @@
+local spawnedPeds = {}
+
+CreateThread(function()
+    while true do
+        Wait(500)
+        for k,v in pairs(Config.ShopLocations) do
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            local distance = #(playerCoords - v.npccoords.xyz)
+
+            if distance < Config.DistanceSpawn and not spawnedPeds[k] then
+                local spawnedPed = NearPed(v.npcmodel, v.npccoords)
+                spawnedPeds[k] = { spawnedPed = spawnedPed }
+            end
+            
+            if distance >= Config.DistanceSpawn and spawnedPeds[k] then
+                if Config.FadeIn then
+                    for i = 255, 0, -51 do
+                        Wait(50)
+                        SetEntityAlpha(spawnedPeds[k].spawnedPed, i, false)
+                    end
+                end
+                DeletePed(spawnedPeds[k].spawnedPed)
+                spawnedPeds[k] = nil
+            end
+        end
+    end
+end)
+
+function NearPed(npcmodel, npccoords)
+    RequestModel(npcmodel)
+    while not HasModelLoaded(npcmodel) do
+        Wait(50)
+    end
+    spawnedPed = CreatePed(npcmodel, npccoords.x, npccoords.y, npccoords.z - 1.0, npccoords.w, false, false, 0, 0)
+    SetEntityAlpha(spawnedPed, 0, false)
+    SetRandomOutfitVariation(spawnedPed, true)
+    SetEntityCanBeDamaged(spawnedPed, false)
+    SetEntityInvincible(spawnedPed, true)
+    FreezeEntityPosition(spawnedPed, true)
+    SetBlockingOfNonTemporaryEvents(spawnedPed, true)
+    -- set relationship group between npc and player
+    SetPedRelationshipGroupHash(spawnedPed, GetPedRelationshipGroupHash(spawnedPed)) -- SetPedRelationshipGroupHash
+    SetRelationshipBetweenGroups(1, GetPedRelationshipGroupHash(spawnedPed), `PLAYER`) -- SetRelationshipBetweenGroups
+    if Config.Debug then
+        local relationship = GetRelationshipBetweenGroups(GetPedRelationshipGroupHash(spawnedPed), `PLAYER`) -- GetRelationshipBetweenGroups
+        print(relationship)
+    end
+    -- end of relationship group
+    if Config.FadeIn then
+        for i = 0, 255, 51 do
+            Wait(50)
+            SetEntityAlpha(spawnedPed, i, false)
+        end
+    end
+    return spawnedPed
+end
+
+-- cleanup
+AddEventHandler("onResourceStop", function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then return end
+    for k,v in pairs(spawnedPeds) do
+        DeletePed(spawnedPeds[k].spawnedPed)
+        spawnedPeds[k] = nil
+    end
+end)
