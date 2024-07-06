52914580457b9ad960a16881592023b74e956a42
Szileni <szileni97@gmail.com>
2024-07-06 21:09:02 +0200

    Update fxmanifest.lua
    + Removed warmenu
    + Version pump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,31 +1,33 @@
-fx_version "adamant"
-
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
 
-games {"rdr3"}
-
-version '1.0.0'
-
-client_scripts {
-    'client/warmenu.lua',
-    'client/client.lua',
-    'config.lua',
-	'client/npcs.lua',
-}
+description 'tbrp-companions'
+version '1.0.1'
 
 shared_scripts {
+    '@ox_lib/init.lua',
     '@rsg-core/shared/locale.lua',
     'locales/en.lua', -- Change to your language
     'config.lua',
 }
 
+client_scripts {
+    'client/client.lua',
+    'config.lua',
+	'client/npcs.lua',
+}
+
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'config.lua',
     'server/*.lua',
-	'@oxmysql/lib/MySQL.lua',
 }
 
 dependencies {
     'rsg-core',
+    'rsg-target',
     'ox_lib'
 }
+
+lua54 'yes'
