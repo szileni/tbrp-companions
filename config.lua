ef4553e35f47d2eb0289dfe69f72f4faab4fb6d6
Szileni <szileni97@gmail.com>
2024-07-06 21:08:17 +0200

    Update config.lua
    + Added 2 petshops (Valentine, Blackwater)
    + Added opening hours or AlwaysOpen
    + Added NPC scenario (you can change it for each npc)

--- config.lua
+++ config.lua
@@ -6,13 +6,20 @@ Config.Debug = false
 Config.FadeIn = true
 Config.DistanceSpawn = 20.0
 Config.EnableTarget = true --For Pet Shop NPC
+Config.Img = "rsg-inventory/html/images/"
+
+---------------------------------
+-- openting hours
+---------------------------------
+Config.AlwaysOpen = false -- if false configure the open/close times
+Config.OpenTime = 8 -- store opens
+Config.CloseTime = 20 -- store closes
 
 Config.KeyBind = 'E'
 
 Config.CallPetKey = true --Set to true to use the CallPet hotkey below
 
 Config.TriggerKeys = {
-    OpenShop = 'E',
     CallPet = 'U',
 }
 
@@ -45,7 +52,7 @@ Config.NoFear = true --Set this to true if you are using Bears/Wolves as pets so
 --}
 
 Config.SearchRadius = 50.0 -- How far the pet will search for a hunted animal. Always a float value i.e 50.0
-Config.FeedInterval = 1000 -- How often in seconds the pet will want to be fed
+Config.FeedInterval = 1800 -- 1800 = 30 min, How often in seconds the pet will want to be fed
 
 Config.RaiseAnimal = true -- If this is enabled, you will have to feed your animal for it to gain XP and grow. Only full grown pets can use commands (halfway you get the Stay command)
 
@@ -64,15 +71,36 @@ Config.Shops = {
     {
         prompt = 'valentine-petshop',
 		Name = Lang:t('label.petshop'),
+        Ring = true,
         ActiveDistance = 1.5,
-        Coords = vector3(-281.20, 684.55, 113.46),
-        Spawndog = vector4( -284.09, 685.34, 113.59, 234.45 ),
-        npcmodel = `mp_u_f_m_nat_traveler_01`,
-        npccoords = vector4(-281.20, 684.55, 113.46, 211.10),
+        Coords = vector3(-285.5119, 658.00457, 113.30006),
+        Spawndog = vector4(-286.3233, 659.20825, 113.41064, 130.15997),
+        npcmodel = `mbh_rhodesrancher_females_01`,
+        npccoords = vector4(-285.5119, 658.00457, 113.30006, 100.1551),
+        npcpetmodel = `A_C_DogHound_01`,
+        npcpetcoords = vector4(-284.7644, 657.09729, 113.21657, 104.9031),
+		scenario = 'MP_LOBBY_STANDING_D',
+        showblip = true,
+    },
+    {
+        prompt = 'blackwater-petshop',
+		Name = Lang:t('label.petshop'),
+        Ring = true,
+        ActiveDistance = 1.5,
+        Coords = vector3(-945.7324, -1226.065, 52.751701),
+        Spawndog = vector4(-947.0184, -1225.372, 52.836936, 192.60287),
+        npcmodel = `u_m_m_bwmstablehand_01`,
+        npccoords = vector4(-945.7324, -1226.065, 52.751701, 185.14344),
         npcpetmodel = `A_C_DogAustralianSheperd_01`,
-        npcpetcoords = vector4(-280.8288, 685.35223, 113.49935, 205.96496),
+        npcpetcoords = vector4(-944.7804, -1226.214, 52.694541, 146.14712),
+		scenario = 'MP_LOBBY_STANDING_C',
         showblip = true,
     }
+}
+
+Config.PetShop = {
+    -- pet shop items
+    [1] = { name = 'raw_meat', price = 3, amount = 500, info = {}, type = 'item', slot = 1, },
 }
 
 Config.PetAttributes = {
@@ -113,6 +141,7 @@ Config.Pets = {
         Text = "$200 - Husky",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_husky.png',
         Param = {
             Price = 200,
             Model = "A_C_DogHusky_01",
@@ -123,6 +152,7 @@ Config.Pets = {
         Text = "$50 - Mutt",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_catahoularcur.png',
         Param = {
             Price = 50,
             Model = "A_C_DogCatahoulaCur_01",
@@ -133,6 +163,7 @@ Config.Pets = {
         Text = "$100 - Labrador Retriever",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_lab.png',
         Param = {
             Price = 100,
             Model = "A_C_DogLab_01",
@@ -143,6 +174,7 @@ Config.Pets = {
         Text = "$100 - Rufus",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_chesbayretriever.png',
         Param = {
             Price = 100,
             Model = "A_C_DogRufus_01",
@@ -153,6 +185,7 @@ Config.Pets = {
         Text = "$150 - Coon Hound",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_bluetickcoonhound.png',
         Param = {
             Price = 150,
             Model = "A_C_DogBluetickCoonhound_01",
@@ -163,6 +196,7 @@ Config.Pets = {
         Text = "$150 - Hound Dog",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_hound.png',
         Param = {
             Price = 150,
             Model = "A_C_DogHound_01",
@@ -173,6 +207,7 @@ Config.Pets = {
         Text = "$200 - Border Collie",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_collie.png',
         Param = {
             Price = 200,
             Model = "A_C_DogCollie_01",
@@ -183,17 +218,18 @@ Config.Pets = {
         Text = "$200 - Poodle",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_poodle.png',
         Param = {
             Price = 200,
             Model = "A_C_DogPoodle_01",
             Level = 1
         }
     },
-    
     {
         Text = "$100 - Foxhound",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_americanfoxhound.png',
         Param = {
             Price = 100,
             Model = "A_C_DogAmericanFoxhound_01",
@@ -204,6 +240,7 @@ Config.Pets = {
         Text = "$100 - Australian Shephard",
         SubText = "",
         Desc = "Best pet you'll ever have",
+		img = 'animal_dog_australianshepherd.png',
         Param = {
             Price = 100,
             Model = "A_C_DogAustralianSheperd_01",
